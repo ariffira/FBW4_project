f223bf9d7aa300d1157ab4e228ec834dfc21925c
ahmad13kurd <evil13kurd@hotmail.com>
2018-10-16 10:26:39 +0200

    refresh

---
+++ assets/ahmadMalla/portfulio.js
@@ -0,0 +1,27 @@
+function facebookover(x) {
+    x.style.color = "#3b5998";
+}
+function facebookout(x) {
+    x.style.color = "#0078d7";
+}
+
+function whatsupover(x) {
+    x.style.color = "#128c7e";
+}
+function whatsupout(x) {
+    x.style.color = "#4aae20";
+}
+
+function twitterover(x) {
+    x.style.color = "#657786";
+}
+function twitterout(x) {
+    x.style.color = "#1da1f2";
+}
+
+function instagramover(x) {
+    x.style.color = "#833ab4";
+}
+function instagramout(x) {
+    x.style.color = "#e1306c";
+}
